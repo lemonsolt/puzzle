bd96adfc2b2de82b6e59de5288d7acbc4eaa9bb0
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-11-21 06:21:29 +0000

    [add]model,controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :users do
+    get 'homes/top'
+  end
+  devise_for :admins
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/show'
+  get 'posts/edit'
+  get 'homes/top'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
