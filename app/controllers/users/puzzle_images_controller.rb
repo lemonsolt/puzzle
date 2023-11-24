6076faab2d2c5cbce00f2e23eb78f69bbe259d18
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-11-24 13:22:45 +0000

    [update]controller

--- app/controllers/users/puzzle_images_controller.rb
+++ app/controllers/users/puzzle_images_controller.rb
@@ -1,13 +1,49 @@
 class Users::PuzzleImagesController < ApplicationController
   def index
-  end
-
-  def show
+    @puzzles = PuzzleImage.all
   end
 
   def new
+    @puzzle = PuzzleImage.new
+  end
+  
+  def create
+    @puzzle = PuzzleImage.new(puzzle_params)
+    @puzzle.user_id = current_user.id
+    if @puzzle.save
+      redirect_to puzzle_images_path
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @puzzle = PuzzleImage.find(params[:id])
   end
 
   def edit
+    @puzzle = PuzzleImage.find(params[:id])
   end
+  
+  def update
+    @puzzle = PuzzleImage.find(params[:id])
+    if @puzzle.update
+      redirect_to puzzle_image_path(@puzzle.id)
+    else
+      render :edit
+    end
+  end
+  
+  def destroy
+    @puzzle = PuzzleImage.find(params[:id])
+    @puzzle.destroy
+    redirect_to puzzle_images_path
+  end
+  
+  protected
+  
+  def puzzle_params
+    params.require(:puzzle_image).permit(:user_id, :title, :image)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,31 +1,12 @@
 Rails.application.routes.draw do
-  namespace :users do
-    get 'puzzle_images/index'
-    get 'puzzle_images/show'
-    get 'puzzle_images/new'
-    get 'puzzle_images/edit'
-  end
+
   devise_for :users
-  namespace :users do
-    get 'puzzules/index'
-    get 'puzzules/new'
-    get 'puzzules/show'
-    get 'puzzules/edit'
-  end
-  namespace :users do
-    get 'posts/index'
-    get 'posts/new'
-    get 'posts/show'
-    get 'posts/edit'
-  end
-  namespace :users do
-    get 'homes/top'
-  end
   devise_for :admins
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/show'
-  get 'posts/edit'
   get 'homes/top'
+
+  resources :puzzle_images, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+    resources :comments,only: [:create, :destroy]
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
