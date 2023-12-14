6bc16655e8d19cfa0e9c888e8e7b369c236560fe
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-12-14 12:18:12 +0000

    [index]

--- app/models/puzzle_image.rb
+++ app/models/puzzle_image.rb
@@ -3,4 +3,12 @@ class PuzzleImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :comments, dependent: :destroy
+  
+  def image_or_empty
+    if self.image.attached? == false
+      return "noimage_camera.jpg"
+    else
+      return image
+    end
+  end
 end
