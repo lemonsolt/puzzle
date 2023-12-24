bd65174aecbc9c5a06b126223b90ec501f295c0e
SaraKinoshita <135306445+lemonsolt@users.noreply.github.com>
2023-12-24 13:52:46 +0000

    [update]route等

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,14 @@ Rails.application.routes.draw do
 
   devise_for :users
   devise_for :admins
-  get 'homes/top'
 
-  resources :puzzle_images, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
-    resources :comments,only: [:create, :destroy]
+  
+  
+  scope module: :users do
+    root to: "homes#top"
+    resources :puzzle_images, only: [:index, :new, :create, :show, :edit, :update, :destroy] do
+      resources :comments,only: [:create, :destroy]
+    end
   end
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
